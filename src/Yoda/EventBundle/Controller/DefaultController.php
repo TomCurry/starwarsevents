6d4b5f57ea4041febe6e5beae3428b08059e3862
Tom Curry <tcurry@ukwebmedia.com>
2015-03-20 12:11:27 +0000

    more of tutorial done, got css and routing fixed

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -6,19 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($firstName, $count)
+    public function indexAction($count, $firstName)
     {
         //$em = $this->container->get('doctrine')->getManager();
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository('EventBundle:Event');
         
         $event = $repo->findOneBy(array(
-            'name' => 'Darth\s surprise birthday party!'
+            'name' => 'Darth\'s surprise birthday party!'
         ));
+        
+        if (!$event) {
+            throw new \Doctrine\ORM\EntityNotFoundException();
+        }
         
         return $this->render(
                 'EventBundle:Default:index.html.twig',
-                array('name' => $firstName, 'count' => $count, 'event' => $event)
+                array(
+                    'name' => $firstName,
+                    'count' => $count,
+                    'event' => $event,
+                )
             );
     }
 }
